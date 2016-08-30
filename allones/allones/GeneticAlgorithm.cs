e4064a3878f1f6e0c650454f97dbefdc2a08c1e6
Jake Bladt <jake@jakebladt.com>
2016-08-30 00:51:28 -0400

    +ctor

--- allones/allones/GeneticAlgorithm.cs
+++ allones/allones/GeneticAlgorithm.cs
@@ -8,5 +8,18 @@ namespace allones
 {
     public class GeneticAlgorithm
     {
+        public int PopulationSize { get; protected set;  }
+        public double MutationRate { get; protected set;  }
+        public double CrossoverRate { get; protected set;  }
+        public int ElitismCount { get; protected set; }
+
+        public GeneticAlgorithm(int ps, double mr, double cr, int ec)
+        {
+            PopulationSize = ps;
+            MutationRate = mr;
+            CrossoverRate = cr;
+            ElitismCount = ec;
+        }
+
     }
 }
