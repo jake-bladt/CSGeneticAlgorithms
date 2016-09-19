f623a69906c6c777abc5b92cbf0d019db789ff04
Jake Bladt <jake@jakebladt.com>
2016-09-19 16:33:23 -0400

    +random binary

--- allones/allones/Individual.cs
+++ allones/allones/Individual.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace allones
 {
-    class Individual
+    public class Individual
     {
+        public int[] Chromosomes { get; set; }
+        public double Fitness { get; set; }
+        public int ChromosomeLength { get
+            {
+                return Chromosomes.Length;
+            } }
+
+        public Individual(int[] chromosomes)
+        {
+            Chromosomes = chromosomes;
+            Fitness = -1.0;
+        }
+
+        public Individual(int chromosomeLength)
+        {
+            Chromosomes = new int[chromosomeLength];
+            // Randomly choose 1s and 0s
+            for(int i = 0; i < chromosomeLength; i++)
+            {
+                Chromosomes[i] = new Random().NextDouble() >= 0.5 ? 1 : 0;
+            }
+        }
+
+
     }
 }
