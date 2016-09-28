80296f5aa525f72ba73c3f2ecfd2ce561eb3bcdd
Jake Bladt <jake@jakebladt.com>
2016-09-28 17:13:50 -0400

    +test framework

---
+++ homers01/HomerPredicter.Tests/PredictiveAlgorithmTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Shouldly;
+using Xunit;
+
+namespace HomerPredicter.Tests
+{
+    public class PredictiveAlgorithmTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
