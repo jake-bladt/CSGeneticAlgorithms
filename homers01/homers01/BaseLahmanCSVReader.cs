0416922ea1812ffdef26d810710c3cc9a550f8ca
Jake Bladt <jake@jakebladt.com>
2016-09-28 14:55:13 -0400

    +config

--- homers01/homers01/BaseLahmanCSVReader.cs
+++ homers01/homers01/BaseLahmanCSVReader.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace homers01
 {
-    class BaseLahmanCSVReader
+    public class BaseLahmanCSVReader
     {
+        public List<Dictionary<String, String>> ParseFile(string path)
+        {
+            var ret = new List<Dictionary<String, String>>();
+            var lines = File.ReadAllLines(path);
+
+            // Lahman stat files start out with a list of columns in the first row.
+            var key = lines[0];
+            var headers = key.Split(',');
+
+            for(int i = 1; i < lines.Length; i++)
+            {
+                var stats = lines[i].Split(',');
+                var dict = new Dictionary<String, String>();
+                for(int j = 0; j < stats.Length; j++)
+                {
+                    dict[headers[j]] = stats[j];
+                }
+                ret.Add(dict);
+            }
+
+            return ret;
+        }
     }
 }
--- homers01/homers01/CsvBattingStatsReader.cs
+++ homers01/homers01/CsvBattingStatsReader.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace homers01
 {
-    public class CsvBattingStatsReader
+    public class CsvBattingStatsReader : BaseLahmanCSVReader
     {
+        public List<BattingStatistics> GetBattingStatistics(string filePath, int mostRecentYear, int yearsBack)
+        {
+            var ret = new List<BattingStatistics>();
+
+            var startYear = mostRecentYear - yearsBack;
+            var allStats = ParseFile(filePath);
+            allStats.ForEach(line =>
+            {
+                var lineYear = Int32.Parse(line["yearId"]);
+                if(lineYear <= mostRecentYear && lineYear >= startYear)
+                {
+                    var lineStat = new BattingStatistics
+                    {
+                        Year = lineYear,
+                        PlayerKey = line["playerID"],
+                        HomeRuns = Int32.Parse(line["HR"])
+                    };
+                    ret.Add(lineStat);
+                }
+            });
+
+            return ret;
+        }
+
+
     }
 }
