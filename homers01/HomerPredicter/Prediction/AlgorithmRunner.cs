75b175da5d162b05d4fb64158c2d0cf4a243e384
Jake Bladt <jake@jakebladt.com>
2016-09-28 19:13:50 -0400

    +runner

---
+++ homers01/HomerPredicter/Prediction/AlgorithmRunner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using HomerPredicter.Models;
+
+namespace HomerPredicter.Prediction
+{
+    public class AlgorithmRunner
+    {
+        protected PredictiveAlgorithm _Algo;
+
+        public AlgorithmRunner(PredictiveAlgorithm algo)
+        {
+            _Algo = algo;
+        }
+
+
+
+    }
+}
